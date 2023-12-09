d4265cf91c9fd01851d6518623aba3042919d5e8
Walker Arce <wsarcera@gmail.com>
2023-12-09 12:40:00 -0600

    Plot curves and update script

--- Flight Payload/Perovskite Performance/perovskite_plot.py
+++ Flight Payload/Perovskite Performance/perovskite_plot.py
@@ -15,10 +15,10 @@ def adc_to_current(adc_code, gain=200.0, ref_voltage=3.29, max_bin=2**8, sense_r
 
 pixel_area = 0.058
 current_density = 100
-gaas_performance = r"C:\Users\jmurc\Documents\GitHub\Big-Red-Sat-1\Flight Payload\Perovskite Performance\GaAs_Assembled\6Dec23.txt"
-p1_performance = r"C:\Users\jmurc\Documents\GitHub\Big-Red-Sat-1\Flight Payload\Perovskite Performance\P1_Assembled\6Dec23.txt"
-p2_performance = r"C:\Users\jmurc\Documents\GitHub\Big-Red-Sat-1\Flight Payload\Perovskite Performance\P2_Assembled\6Dec23.txt"
-p3_performance = r"C:\Users\jmurc\Documents\GitHub\Big-Red-Sat-1\Flight Payload\Perovskite Performance\P3_Assembled\6Dec23.txt"
+gaas_performance = r"GaAs_Assembled\GaAs_PostVibe\9Dec23.txt"
+p1_performance = r"P1_Assembled\X018_PostVibe\9Dec2023.txt"
+p2_performance = r"P2_Assembled\X016_PostVibe\9Dec2023.txt"
+p3_performance = r"P3_Assembled\X002_PostVibe\9Dec23.txt"
 
 
 def plot_save_curves(curve_file, perov):
@@ -35,20 +35,20 @@ def plot_save_curves(curve_file, perov):
         curve = lines[i:i+18]
         if "DOWN" in curve[0]:
             curve_points = [[int(x.split(",")[1].split(":")[1]), int(x.split(",")[2].split(":")[1])] for x in curve[2:]]
-            voltage = [adc_to_voltage(x[0], gain=0.5 if perov else 0.25) for x in curve_points]
+            voltage = [adc_to_voltage(x[0], gain=1 if perov else 0.25) for x in curve_points]
             current = [adc_to_current(x[1]) / pixel_area for x in curve_points]
             if perov:
-                voltage = voltage[14:] + voltage[:14]
-                current = current[14:] + current[:14]
+                voltage = voltage[10:] + voltage[:10]
+                current = current[10:] + current[:10]
             reverse_curves.append([voltage, current])
             ax[0].plot(voltage, current)
         if "UP" in curve[0]:
             curve_points = [[int(x.split(",")[1].split(":")[1]), int(x.split(",")[2].split(":")[1])] for x in curve[2:]]
-            voltage = [adc_to_voltage(x[0],  gain=0.5 if perov else 0.25) for x in curve_points]
+            voltage = [adc_to_voltage(x[0],  gain=1 if perov else 0.25) for x in curve_points]
             current = [adc_to_current(x[1]) / pixel_area for x in curve_points]
             if perov:
-                voltage = voltage[2:] + voltage[0:2]
-                current = current[2:] + current[0:2]
+                voltage = voltage[6:] + voltage[0:6]
+                current = current[6:] + current[0:6]
             forward_curves.append([voltage, current])
             ax[1].plot(voltage, current)
 
@@ -60,7 +60,7 @@ def plot_save_curves(curve_file, perov):
     ax[0].set_title("Reverse JV Curves")
     figure = plt.gcf()
     figure.set_size_inches(20, 10)
-    plt.savefig(os.path.join(pathlib.Path(curve_file).parent, "plot.png"), bbox_inches='tight', dpi=100)
+    plt.savefig(os.path.join(pathlib.Path(curve_file).parent, "plot_" + pathlib.Path(curve_file).stem + ".png"), bbox_inches='tight', dpi=100)
     plt.show()
 
 
